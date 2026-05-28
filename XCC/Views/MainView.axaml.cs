22ae319454a621383faaacf1afc3a436539730a0
Thiriob <gaspard.thirion@gmail.com>
2026-05-29 01:17:04 +0200

    views ok

--- XCC/Views/MainView.axaml.cs
+++ XCC/Views/MainView.axaml.cs
@@ -1,3 +1,5 @@
+using System;
+using Avalonia;
 using Avalonia.Controls;
 
 namespace XCC.Views;
@@ -7,5 +9,12 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
+    }
+
+    protected override void OnSizeChanged(SizeChangedEventArgs e)
+    {
+        base.OnSizeChanged(e);
+        if (e.NewSize.Height > 0)
+            ContentArea.Width = Math.Min(e.NewSize.Width, e.NewSize.Height * 9.0 / 16.0);
     }
 }
